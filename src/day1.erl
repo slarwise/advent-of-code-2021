41aaede0f9057b28d1efe34158448461d2859beb
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-12-12 14:14:40 +0100

    Do day 1 part 1

--- src/day1.erl
+++ src/day1.erl
@@ -1,7 +1,45 @@
+-include_lib("eunit/include/eunit.hrl").
+
 -module(day1).
 
 -export([run/1]).
 
 -spec run(Part :: 1..2) -> ok.
-run(Part) ->
-    io:format("Running day 1, part ~p~n", [Part]).
+run(1) ->
+    Report = read_input(),
+    Increases = count_increases(Report),
+    io:format("The number of increases is ~p~n", [Increases]).
+
+count_increases(Report) ->
+    {Increases, _LastValue} =
+    lists:foldl(fun(Current, {Increases, Previous}) ->
+                        case Current > Previous of
+                            true -> {Increases + 1, Current};
+                            false -> {Increases, Current}
+                        end
+                end,
+                {0, infinity},
+                Report
+               ),
+    Increases.
+
+count_increases_test() ->
+    Report = [
+              199,
+              200,
+              208,
+              210,
+              200,
+              207,
+              240,
+              269,
+              260,
+              263
+             ],
+    ?assertEqual(7, count_increases(Report)).
+
+read_input() ->
+    Filename = "input/day1",
+    {ok, Binary} = file:read_file(Filename),
+    StringList = string:lexemes(binary_to_list(Binary), "\n"),
+    lists:map(fun list_to_integer/1, StringList).
